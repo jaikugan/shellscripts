9264109c40181b4e8890d745d391e2635d7990b3
jaikugan <kuganpositive@gmail.com>
2024-08-06 20:16:17 +0530

    changes added

---
+++ node_health_check.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+########################################
+# Node_Health_check
+########################################
+
+# Orginal Developer : Kugan M
+# Date : 06 Aug 2024
+# Version : V1
+# This script will collect metrics of cpu and memory
+
+df -h
+
+free -h
+
+nproc
